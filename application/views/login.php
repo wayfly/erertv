5cf15fb1fcd0236d33c49a4217a69b4cd01d4078
wb-lff227748 <wb-lff227748@alibaba-inc.com>
2016-11-03 17:47:53 +0800

    注册页面样式修改

--- application/views/login.php
+++ application/views/login.php
@@ -1,20 +1,79 @@
 <html>
-<head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"></head>
+<head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>二二视频网站--登陆页面</title>
+    <!-- Bootstrap Core CSS -->
+    <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- MetisMenu CSS -->
+    <link href="../bootstrap/css/metisMenu.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="../bootstrap/css/sb-admin-2.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="../bootstrap/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
 <?php echo validation_errors(); ?>
-<?php echo form_open('login/formsubmit'); ?>
-<table>
-	<tr>
-		<td>用户名</td>
-		<td><input type="text" name="username"></td>
-	</tr>
-	<tr>
-		<td>密码</td>
-		<td><input type="password" name="password"></td>
-	</tr>
-	<tr>
-		<td><input type="submit" name="submit" value="login"></td>
-		<td><input type="submit" name="submit" value="register"></td>
-	</tr>
-</table>
-</form>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-md-offset-4">
+                <div class="login-panel panel panel-default">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">请 注册</h3>
+                    </div>
+                    <div class="panel-body">
+                        <form role="form" action="formsubmit" method="POST">
+                            <fieldset>
+                                <div class="form-group">
+                                    <input class="form-control" placeholder="邮箱" name="email" type="email" autofocus>
+                                </div>
+                                <div class="form-group">
+                                    <input class="form-control" placeholder="密码" name="password" type="password" value="">
+                                </div>
+                                <div class="checkbox">
+                                    <label>
+                                        <input name="remember" type="checkbox" value="记住我">记住我
+                                    </label>
+                                </div>
+                                <!-- Change this to a button or input when using this as a form -->
+								<div class="form-group">
+									<input type="submit" name="submit" value="register" class="btn btn-lg btn-success btn-block">
+									<!--input type="submit" name="submit" value="register" class="btn btn-lg btn-success btn-block"-->
+									<!--a href="formsubmit" class="btn btn-lg btn-success btn-block">注册</a-->
+								</div>
+                            </fieldset>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- jQuery -->
+    <script src="../bootstrap/js/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="../bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="../bootstrap/js/metisMenu.min.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="../bootstrap/js/sb-admin-2.js"></script>
+
+</body>
 </html>
